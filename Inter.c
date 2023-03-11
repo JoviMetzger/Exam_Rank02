21f36a8b9c2d03422f68bc6bb3f8a928c2630fd4
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-03-11 12:38:55 +0100

    Update Inter.c

--- Inter.c
+++ Inter.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include <unistd.h>
 
 int main(int argc, char **argv)
 {
-    char map[255];
+    char tmp[255];
     int i;
     int j;
 
@@ -15,10 +14,10 @@ int main(int argc, char **argv)
             j = 0;
             while (argv[2][j])
             {
-                if (map[(int)argv[1][i]] != 1 && (argv[1][i] == argv[2][j]))
+                if (tmp[(int)argv[1][i]] != 1 && (argv[1][i] == argv[2][j]))
                 {
                     write(1, &argv[1][i], 1);
-                    map[(int)argv[1][i]] = 1;
+                    tmp[(int)argv[1][i]] = 1;
                 }
                 j++;
             }
